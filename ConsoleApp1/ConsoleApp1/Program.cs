04b409f0bdd33c1f023c069ffbcc6b39aa924428
NijatAziz <nijatfaz@code.edu.az>
2023-07-18 18:57:08 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Console.WriteLine("Faiz derecesini daxil edin:");
+//if (!double.TryParse(Console.ReadLine(), out double faiz_derecesi))
+//{
+//    Console.WriteLine("Invalid input. Reqemsal deyer daxil edin.");
+//    return;
+//}
+
+//int[] numbers = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+//int sum = 0;
+
+//foreach (var number in numbers)
+//{
+//    if (number < 20)
+//    {
+//        sum += number + (int)(number * faiz_derecesi / 100);
+//    }
+//}
+
+//Console.WriteLine("Faiz tetbiq edildikden sonra elementlerin cemi 20-den azdir: " + sum);
+        
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Console.WriteLine("Enter a number:");
+//if (!int.TryParse(Console.ReadLine(), out int index))
+//{
+//    Console.WriteLine("Yanlıs number daxiletmesi!");
+//    return;
+//}
+
+//Console.WriteLine("Enter a word:");
+//string word = Console.ReadLine();
+
+//if (index >= 0 && index < word.Length)
+//{
+//    word = word.Remove(index, 1);
+//    Console.WriteLine("Result: " + word);
+//}
+//else
+//{
+//    Console.WriteLine("Yanlıs indeks! İndeks 0 ile sozun uzunlugu arasinda olmalidir.");
+//}
+    
